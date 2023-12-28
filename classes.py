32c806e3f91aeb6ca84f51e93666b49b11adfa2a
Malam <60240119+Malamdg@users.noreply.github.com>
2023-12-28 18:30:11 +0100

    wip implement game

--- classes.py
+++ classes.py
@@ -29,21 +29,24 @@ class Deck:
 
 
 class TrainColor(Enum):
-    NONE = 0
-    RED = 12
-    BLUE = 12
-    GREEN = 12
-    YELLOW = 12
-    ROSE = 12
-    WHITE = 12
-    BLACK = 12
-    JOKER = 14
+    BLACK = "black", 12
+    BLUE = "blue", 12
+    GREEN = "green", 12
+    JOKER = "joker", 14
+    ORANGE = "orange", 12
+    PINK = "pink", 12
+    RED = "red", 12
+    YELLOW = "yellow", 12
+    WHITE = "white", 12
 
 
 class TrainCard(Card):
     def __init__(self, color: TrainColor):
         super().__init__()
         self.color = color
+
+    def __str__(self):
+        return self.color.value[0]
 
 
 class ObjectiveCard(Card):
@@ -54,14 +57,87 @@ class ObjectiveCard(Card):
         self.points = points
 
 
+class CityEnum(Enum):
+    ATLANTA = "Atlanta"
+    BOSTON = "Boston"
+    CALGARY = "Calgary"
+    CHICAGO = "Chicago"
+    DALLAS = "Dallas"
+    DENVER = "Denver"
+    DULUTH = "Duluth"
+    EL_PASO = "El Paso"
+    HELENA = "Helena"
+    HOUSTON = "Houston"
+    KANSAS_CITY = "Kansas City"
+    LITTLE_ROCK = "Little Rock"
+    LOS_ANGELES = "Los Angeles"
+    MIAMI = "Miami"
+    MONTREAL = "Montréal"
+    NASHVILLE = "Nashville"
+    NEW_ORLEANS = "New Orleans"
+    NEW_YORK = "New York"
+    OKLAHOMA_CITY = "Oklahoma City"
+    PHOENIX = "Phoenix"
+    PITTSBURGH = "Pittsburgh"
+    PORTLAND = "Portland"
+    SALT_LAKE_CITY = "Salt Lake City"
+    SAN_FRANCISCO = "San Fransisco"
+    SANTA_FE = "Santa Fe"
+    SAULT_STE_MARIE = "Sault Ste. Marie"
+    SEATTLE = "Seattle"
+    TORONTO = "Toronto"
+    VANCOUVER = "Vancouver"
+    WINNIPEG = "Winnipeg"
+
+
 class ObjectiveCardsDeck(Deck):
     def __init__(self):
-        super().__init__([])
+        super().__init__(
+            [
+                ObjectiveCard(CityEnum.HELENA, CityEnum.LOS_ANGELES, 8),
+                ObjectiveCard(CityEnum.SAULT_STE_MARIE, CityEnum.NASHVILLE, 8),
+                ObjectiveCard(CityEnum.TORONTO, CityEnum.MIAMI, 10),
+                ObjectiveCard(CityEnum.CHICAGO, CityEnum.SANTA_FE, 9),
+                ObjectiveCard(CityEnum.KANSAS_CITY, CityEnum.HOUSTON, 5),
+                ObjectiveCard(CityEnum.NEW_YORK, CityEnum.ATLANTA, 6),
+                ObjectiveCard(CityEnum.WINNIPEG, CityEnum.LITTLE_ROCK, 11),
+                ObjectiveCard(CityEnum.LOS_ANGELES, CityEnum.NEW_YORK, 21),
+                ObjectiveCard(CityEnum.DENVER, CityEnum.PITTSBURGH, 11),
+                ObjectiveCard(CityEnum.DENVER, CityEnum.EL_PASO, 4),
+                ObjectiveCard(CityEnum.PORTLAND, CityEnum.NASHVILLE, 17),
+                ObjectiveCard(CityEnum.SAN_FRANCISCO, CityEnum.ATLANTA, 17),
+                ObjectiveCard(CityEnum.SEATTLE, CityEnum.NEW_YORK, 22),
+                ObjectiveCard(CityEnum.LOS_ANGELES, CityEnum.MIAMI, 20),
+                ObjectiveCard(CityEnum.SEATTLE, CityEnum.LOS_ANGELES, 9),
+                ObjectiveCard(CityEnum.DULUTH, CityEnum.HOUSTON, 8),
+                ObjectiveCard(CityEnum.MONTREAL, CityEnum.ATLANTA, 9),
+                ObjectiveCard(CityEnum.MONTREAL, CityEnum.NEW_ORLEANS, 13),
+                ObjectiveCard(CityEnum.CALGARY, CityEnum.PHOENIX, 13),
+                ObjectiveCard(CityEnum.DALLAS, CityEnum.NEW_YORK, 11),
+                ObjectiveCard(CityEnum.VANCOUVER, CityEnum.SANTA_FE, 13),
+                ObjectiveCard(CityEnum.WINNIPEG, CityEnum.HOUSTON, 12),
+                ObjectiveCard(CityEnum.CALGARY, CityEnum.SALT_LAKE_CITY, 7),
+                ObjectiveCard(CityEnum.VANCOUVER, CityEnum.MONTREAL, 20),
+                ObjectiveCard(CityEnum.LOS_ANGELES, CityEnum.CHICAGO, 16),
+                ObjectiveCard(CityEnum.PORTLAND, CityEnum.PHOENIX, 11),
+                ObjectiveCard(CityEnum.SAULT_STE_MARIE, CityEnum.OKLAHOMA_CITY, 9),
+                ObjectiveCard(CityEnum.DULUTH, CityEnum.EL_PASO, 10),
+                ObjectiveCard(CityEnum.CHICAGO, CityEnum.NEW_ORLEANS, 7),
+                ObjectiveCard(CityEnum.BOSTON, CityEnum.MIAMI, 12)
+            ]
+        )
 
 
 class TrainCardsDeck(Deck):
-    def __init__(self):
-        super().__init__([])
+    def __init__(self, empty: bool):
+        if empty:
+            super().__init__([])
+        else:
+            train_cards = []
+            for color in TrainColor:
+                for _ in range(color.value[1]):
+                    train_cards.append(TrainCard(color))
+            super().__init__(train_cards)
 
 
 class PlayerColors(Enum):
@@ -74,3 +150,75 @@ class PlayerColors(Enum):
     GREEN = auto()
     BLACK = auto()
 
+
+class Pawn:
+    def __init__(self, _color: PlayerColors):
+        self.color = _color
+
+
+class Player:
+    def __init__(self, _color: PlayerColors, _order: int):
+        self.color = _color
+        self.cards = TrainCardsDeck(empty=True)
+        self.objectives = ObjectiveCardsDeck()
+        self.pawns = [Pawn(self.color) for _ in range(45)]
+        self.score = 0
+        self.order = _order
+        self.str_type = "User"
+        self.completed_objectives_count = 0
+
+    def __str__(self):
+        return f"Player #{self.order} ({self.str_type}) color : {self.color.value}"
+
+    def __gt__(self, other):
+        # Order for first player list sort
+        if self.score == other.score and self.score == 0:
+            return self.order > other.score
+
+        if self.score != other.score:
+            return self.score > other.score
+
+        if self.score == other.score:
+            if self.completed_objectives_count != other.completed_objectives_count:
+                return self.completed_objectives_count > other.completed_objectives_count
+
+            else:
+                # Longest railway
+                pass
+
+    def __le__(self, other):
+        return not (self > other)
+
+
+class AIPlayer(Player):
+    def __init__(self, _color: PlayerColors, _order: int):
+        super().__init__(_color, _order)
+        self.str_type = "AI"
+
+
+class Game:
+    def __init__(self, n_player: int, n_ai: int):
+        self.player_total = n_player
+        self.ai_count = n_ai
+
+        self.train_cards_deck = TrainCardsDeck(empty=False)
+        self.discarded_train_cards = TrainCardsDeck(empty=True)
+        self.objective_cards_deck = ObjectiveCardsDeck()
+        self.players = []
+
+        self.init_players()
+
+    def init_players(self):
+        n_players = self.player_total - self.ai_count
+        color_list = [color for color in PlayerColors]
+        order_list = [i+1 for i in range(self.player_total)]
+        rd.shuffle(color_list)
+        rd.shuffle(order_list)
+        for i in range(n_players):
+            self.players.append(Player(color_list.pop(), order_list[i]))
+
+        for i in range(self.ai_count):
+            self.players.append(AIPlayer(color_list.pop(), order_list[i]))
+
+        for player in self.players:
+            print(player)
--- main.py
+++ main.py
@@ -1,5 +1,77 @@
-from classes import PlayerColors
+from classes import Game
+
+
+def main():
+    print("=============== Ticket To Ride =================")
+    user_input = str(input("Start game ? (y/n) \n"))
+    # Setup game
+    player_count_ok = False
+    n_player = -1
+
+    while not player_count_ok:
+        if user_input.lower().strip() != "y":
+            return
+        n_player = int(input("How much players ? : "))
+
+        # -- Input check --
+        player_count_ok = 2 <= n_player <= 5
+
+        if not player_count_ok:
+            # Case 1 : Too many players selected
+            if n_player > 5:
+                user_input = str(
+                    input(f"Too much players selected : {n_player} starting the game with 5 players ? (y/n) \n")
+                )
+                n_player = 5
+
+                if user_input.lower().strip() != "y":
+                    user_input = str(input("Continue ? (y/n) \n"))
+                    if user_input.lower().strip() != "y":
+                        return
+            # Case 2 : Too few player selected
+            else:
+                print("Too few players selected")
+                user_input = str(input("Continue ? (y/n) \n"))
+                if user_input.lower().strip() != "y":
+                    return
+
+    ai_count_ok = False
+    n_ai = -1
+    while not ai_count_ok:
+        n_ai = int(input("How much AI players ? : "))
+
+        if n_ai < 0:
+            n_ai = 0
+
+        # -- Input check --
+        ai_count_ok = n_player >= n_ai
+
+        # Too many AI players selected
+        if n_ai > n_player:
+            user_input = str(
+                input(f"Too much AI players selected : {n_ai}, would you like to reselect AI players count ? (y/n) \n")
+            )
+            n_ai = 0
+
+            if user_input.lower().strip() != "y":
+                user_input = str(input("Continue with no AI player ? (y/n) \n"))
+                if user_input.lower().strip() == "y":
+                    ai_count_ok = True
+
+    n = n_player - n_ai
+    message = f"{n} players and {n_ai} AI players"
+
+    if n == 0:
+        message = f"{n_player} AI players"
+
+    if n == n_player:
+        message = f"{n_player} players"
+
+    print(f"Starting game with {message}")
+
+    game = Game(n_player, n_ai)
+    print(f"game.player_total = {game.player_total}")
+
 
 if __name__ == '__main__':
-    for playerColor in PlayerColors:
-        print(f"{playerColor.name} = {playerColor.value}")
+    main()
